807e3017b4003e00e56a7e67818d952e59fe505d
Lucas Burg <lucasburgmota@gmail.com>
2017-10-19 04:11:06 -0200

    Implementado as funções de consulta

--- bin/main.go
+++ bin/main.go
@@ -2,13 +2,60 @@ package main
 
 import (
 	"html/template"
-	//"io/ioutil"
 	"net/http"
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+	"fmt"
 )
 
+type Scorecard struct {
+	unitid string
+	opeid string
+	opeid6 string
+	instnm string
+	city string
+	stabbr string
+	insturl string
+}
+
+var rows_scorecard []Scorecard
+
+func listScorecard(sct_column_search string, txt_search string) []Scorecard {
+	db, err := sql.Open("postgres", "user=postgres dbname=transparency password=99253164 sslmode=disable")
+	if err != nil {
+		log.Fatal("Error: parametros invalidos para a conexão com o database")
+	}
+	query := fmt.Sprintf("select unitid from scorecard where %s = '%s' order by stabbr, city, instnm asc limit 100", sct_column_search, txt_search)
+	//fmt.Println(query)
+	rows, err := db.Query(query)
+	if err != nil {
+	  log.Fatal(err)
+	}
+	for rows.Next() {
+
+		//fmt.Println(rows_scorecard)
+
+		var s Scorecard
+		err = rows.Scan(&s)
+		
+		fmt.Println(err)
+
+		rows_scorecard = append(rows_scorecard, s)
+	}
+
+	//fmt.Println(rows_scorecard)
+
+	return rows_scorecard
+}
+
 func scorecardSearchHandler(w http.ResponseWriter, r *http.Request) {
+	sct_column_search := r.FormValue("sct_column_search")
+	txt_search := r.FormValue("txt_search")
+	list := listScorecard(sct_column_search, txt_search)
 	t, _ := template.ParseFiles("template/table.html")
-    t.Execute(w, nil)
+	//fmt.Println(list)
+    t.Execute(w, list)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,5 +67,6 @@ func main() {
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))))
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/scorecard_search", scorecardSearchHandler) 
+	fmt.Println("start on localhost:8000")
 	http.ListenAndServe(":8000", nil)
 }
